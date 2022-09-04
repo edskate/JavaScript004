949fc65c8de4676f1e2f2b438256a46e5e177c4f
Edilson Gomes de lima <edskateicevida@gmail.com>
2022-09-04 00:51:14 -0300

    Javascript 005
     javascript

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-document.body.innerText = 'hello world'
+// ⌚️ object
+
+
+const user = {
+name: 'ed',
+nickname:'schell',
+idade: 47,
+address: {
+street: 'rua text',
+number: 234,
+},
+};
+
+// desextruturação
+
+//Rest operator
+
+
+
+function mostraIdade({idade}) {
+return idade;
+}
+
+//operador de resto
+
+//const { name, idade, ...rest } = user;
+
+
+
+const arrays = [1,2,3,4,5,6,7,8,9,10]
+
+const[first, second, ...rest] = arrays;
+
+
+document.body.innerText =JSON.stringify({first,second, rest})
+
